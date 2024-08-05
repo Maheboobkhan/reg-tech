331ce577ba58903710f26ef85fdb5501c6a4c1c5
khansaifkhan <khansaifurrhaman@gmail.com>
2024-08-05 18:14:14 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -111,7 +111,7 @@ class Footer extends React.Component {
 
         return (
             <footer className="pt-8 bg-gray-100 text-gray-800 border-t border-gray-400 w-full">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 font-myfont">
+                <div className="hidden container mx-auto md:grid md:grid-cols-4 md:gap-8 font-myfont">
 
                     {/* Contact Details */}
                     <div className="space-y-4 flex flex-col mx-auto">
@@ -167,6 +167,72 @@ class Footer extends React.Component {
                         </ul>
                     </div>
                 </div>
+
+
+                {/* for mobile */}
+                <div className="md:hidden container flex flex-col font-myfont">
+
+                    {/* Contact Details */}
+                    <div className='flex'>
+                        <div className="space-y-3 flex pl-3 flex-col mx-auto">
+                            <h2 className="text-lg font-bold text-[#e27daa] font-myfontbold">Contact Details</h2>
+                            <p className='text-[13px] md:text-lg'>Feel free to get in touch with us</p>
+
+                            <div className="flex items-center">
+                                <MdOutlineCall className="mr-2 text-[#e27daa]" style={{ fontSize: '1.5rem' }} />
+                                <a href="tel:+1234567890" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">+91 7766969646</a>
+                            </div>
+
+                            <div className="flex items-center">
+                                <GoMail className="mr-2 text-[#e27daa]" style={{ fontSize: '1.5rem' }} />
+                                <a href="mailto:info@example.com" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">info@docboyz.in</a>
+                            </div>
+                        </div>
+
+                        {/* Our Services */}
+                        <div className="flex flex-col mx-auto">
+                            <h2 className="text-lg font-bold text-[#e27daa] font-myfontbold">Our Services</h2>
+                            <ul className='flex flex-col gap-y-3'>
+                                <li><Link to="/e-nach" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">e-Nach/e-Mandate</Link></li>
+                                <li><Link to="/account_verification" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Bank Account Verification</Link></li>
+                                <li><Link to="" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg" onClick={() => this.handleLinkClick(shouldHideFooter, '/', 'verified-customer-acquisition')}>Verified Customer Acquisition</Link></li>
+                                <li><Link to="" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg" onClick={() => this.handleLinkClick(shouldHideFooter, '/', 'vehicle-verification')}>Vehicle Verification</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Postman & API */}
+
+
+                    <div className='flex mt-10'>
+                        <div className="space-y-3 flex flex-col mx-auto">
+                            <h2 className="font-myfontbold font-bold text-lg text-[#e27daa]">Postman & API</h2>
+                            <ul className='flex flex-col gap-y-2'>
+                                <li><Link to="/customer_verification" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Customer Verification</Link></li>
+                                <li><Link to="/e_kyc" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">E-Kyc</Link></li>
+                                <li><Link to="/video_kyc" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Video KYC</Link></li>
+                                <li><Link to="/e_sign" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">E-Sign</Link></li>
+                                <li><Link to="/offline_aadhar" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Offline Aadhar</Link></li>
+                                <li><Link to="/aadhar_masking" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Aadhar Masking</Link></li>
+                                <li><Link to="/face-match" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">DB Fmatch</Link></li>
+                            </ul>
+                        </div>
+
+                        {/* Quick Links */}
+                        <div className="space-y-3 flex flex-col mx-auto">
+                            <h2 className="font-myfontbold font-bold text-lg text-[#e27daa]">Quick Links</h2>
+                            <ul className='flex flex-col gap-y-2'>
+                                <li><Link to="/company" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">About Us</Link></li>
+                                <li><Link to="/blog" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Blog</Link></li>
+                                <li><Link to="/careers" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Careers</Link></li>
+                                <li><Link to="/contact-us" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Contact Us</Link></li>
+                                <li><Link to="/contact-us" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Media</Link></li>
+                                <li><Link to="/company" className="hover:underline hover:text-[#e27daa] text-[13px] md:text-lg">Industries We Serve</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
 
                 {/* Social Media Icons */}
                 <div className="text-center mt-6">
